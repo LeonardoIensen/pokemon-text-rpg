12bf5ad8dd51c42e3e850d6a112ec9354b9b1e29
Leonardo Iensen Da Silva <leo8681silva@gmail.com>
2026-08-15 21:52:00 -0300

    Adiciona gerenciamento de equipe ao Treinador

--- trainer.py
+++ trainer.py
@@ -2,3 +2,11 @@ class Trainer:
     def __init__(self, name, pokemon):
         self.name = name
         self.party = [pokemon]
+
+    def add_pokemon(self, pokemon):
+        self.party.append(pokemon)
+
+    def show_party(self):
+        print("--- PARTY ---\n")
+        for i, pokemon in enumerate(self.party, start=1):
+            print(f"{i} - {pokemon.name}")
